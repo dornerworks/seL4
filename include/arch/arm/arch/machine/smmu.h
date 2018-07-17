b2cc280cdddddfd222f3c3a34306f829d91c84ec
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2018-07-17 15:33:13 -0400

    Updated licensing in some code headers.
    Change-Id: Ic25f7372061b28aa9711b80aaa218ee9d8709a48

--- include/arch/arm/arch/machine/smmu.h
+++ include/arch/arm/arch/machine/smmu.h
@@ -1,11 +1,12 @@
 /*
  * Copyright 2016, General Dynamics C4 Systems
+ * Copyright 2018, DornerWorks
  *
  * This software may be distributed and modified according to the terms of
  * the GNU General Public License version 2. Note that NO WARRANTY is provided.
  * See "LICENSE_GPLv2.txt" for details.
  *
- * @TAG(GD_GPL)
+ * @TAG(DATA61_DORNERWORKS_GPL)
  */
 
 #ifndef __ARCH_SMMU_H
